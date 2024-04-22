acebf3b0ff995c4241ecb04ab2591a1bac4f74b9
chloe0524 <chloco31@gmail.com>
2024-04-22 11:57:35 +0200

    added import typing

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 """function 'sum_list' = 'list input_list' returns sum'"""
 
-import math
-
-input_list = list
+from typing import List
 
 
-def sum_list(input_list: float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """return 'list sum' 'float'"""
     return sum(input_list)
