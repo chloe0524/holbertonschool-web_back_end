7ad3bdae72702477607b70e5c428e0095a471d2d
chloe0524 <chloco31@gmail.com>
2024-04-22 16:35:37 +0200

    mistake in firt line fixed

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
-"""
-Coroutine looping 10times and yield a random number
-"""
+"""coroutine called async_generator that takes no arguments"""
 
 import asyncio
 import random
 
 
 async def async_generator():
-    """ Yield a random number """
-    for i in range(10):
+    """coroutine will loop 10 times"""
+    for the_checker_will_be_the_death_of_me in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
