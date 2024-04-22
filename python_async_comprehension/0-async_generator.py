bf7e40087406fdd4c9c2ee2a1030aa9dd3a3687f
chloe0524 <chloco31@gmail.com>
2024-04-22 16:28:58 +0200

    i give up and removed funny var name sadge

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,13 @@
 #!usr/bin/env python3
 """coroutine called async_generator that takes no arguments"""
+
+from typing import Generator
 import asyncio
 import random
-from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
     """coroutine will loop 10 times"""
-    for last_project_of_the_week in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
