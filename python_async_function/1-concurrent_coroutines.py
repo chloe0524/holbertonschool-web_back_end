7e1ca31215e4bda97cdd57c8662349e544c233b1
chloe0524 <chloco31@gmail.com>
2024-04-22 14:30:43 +0200

    used import instead of method from previous task

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,12 +3,12 @@
 
 import asyncio
 import random
-
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """returns 'delay' time."""
     delays = []
     for _ in range(n):
