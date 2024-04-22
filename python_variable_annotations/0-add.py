22a3e2c281d69edf9e45da1770aebc9879e6a76a
chloe0524 <chloco31@gmail.com>
2024-04-22 10:55:51 +0200

    added docstring in method

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""function add = a float + float b"""
+"""function 'add' = 'a float + float b'"""
 
 
 def add(a: float, b: float) -> float:
+    """""return 'a + b'"""
     return float(a+b)
