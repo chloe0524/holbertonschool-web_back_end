aa2028ae10521152d24347f6fccbf827d9d4b946
chloe0524 <chloco31@gmail.com>
2024-04-22 15:27:59 +0200

    measure_time function with integers n and max_delay as arguments that measures the total execution time for wait_n(n, max_delay)

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,14 +3,15 @@
 
 import asyncio
 import random
-import time
+import timeit
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 async def measure_time(n: int, max_delay: int) -> float:
     """returns 'total_time / n'"""
-    start_time = time.time()
+    startingblock = timeit.default_timer()
     await wait_n(n, max_delay)
-    total_time = time.time()-start_time
+    timeover = timeit.default_timer()
+    total_time = timeover - startingblock
     return total_time / n
