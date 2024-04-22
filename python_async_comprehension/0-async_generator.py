e420cf5d962b91a9b1b7e8eeeea0bfb37ad9ec50
chloe0524 <chloco31@gmail.com>
2024-04-22 16:26:47 +0200

    quite literally forgot the generator part of the async generator project/task

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,11 @@
 """coroutine called async_generator that takes no arguments"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """coroutine will loop 10 times"""
     for last_project_of_the_week in range(10):
-        await asyncio.sleep(1) in range(10)
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
