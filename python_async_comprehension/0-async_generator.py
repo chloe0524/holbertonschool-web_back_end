89f582c39605f8dbabc98dc73486937d30c241f1
chloe0524 <chloco31@gmail.com>
2024-04-22 16:31:44 +0200

    removed generator import

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,13 +1,12 @@
 #!usr/bin/env python3
 """coroutine called async_generator that takes no arguments"""
 
-from typing import Generator
 import asyncio
 import random
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator():
     """coroutine will loop 10 times"""
-    for _ in range(10):
+    for the_checker_will_be_the_death_of_me in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
