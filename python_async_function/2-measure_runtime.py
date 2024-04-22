c0e6b3f9854bb23ebdb7299b7de039eb6000cc91
chloe0524 <127857895+chloe0524@users.noreply.github.com>
2024-04-22 17:22:10 +0200

    Update 2-measure_runtime.py remove big docstring

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -9,16 +9,7 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    """
-    Measures the total execution time
-
-    Args:
-        n (int): number of tasks to be executed
-        max_delay (int): maximum delay in seconds for each task
-
-    Return:
-        float: average time per task
-    """
+    """measures the total exec time"""
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     time_over = time.time()
