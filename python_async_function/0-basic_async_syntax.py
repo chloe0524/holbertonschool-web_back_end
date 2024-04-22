33ff8fec41679cbc6bc18b9c6f4bff857a73f62e
chloe0524 <chloco31@gmail.com>
2024-04-22 14:13:29 +0200

    task 0: The  coroutine waits for a random delay up to  seconds, then returns the actual delay time.

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""'wait_random'coroutine 'waits'for 'random''delay' up+returns 'delay time'"""
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> int:
+    """returns 'delay' time."""
+    temp = random.random() * max_delay
+    await asyncio.sleep(temp)
+    return temp
