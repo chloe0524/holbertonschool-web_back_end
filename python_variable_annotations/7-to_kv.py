5a15ffaa9e5209595556c3cc591ea8fdc2efcf3c
chloe0524 <chloco31@gmail.com>
2024-04-22 12:09:49 +0200

    to_kv that takes a string k and an int OR float v as arguments and returns a tuple

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """to_kv takes a 'string k' + an 'int OR float v' return 'tuple'"""
 
+from typing import Union, Tuple
 
-def to_kv(k: str, v: int or float) -> tuple:
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """return 'str' 'int/float'"""
     return k, float(v**2)
