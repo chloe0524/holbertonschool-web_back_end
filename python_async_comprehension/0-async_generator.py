eac138b853b4bf463528a871f4ab4f9d861aee0c
chloe0524 <chloco31@gmail.com>
2024-04-22 16:20:14 +0200

    task 0: The coroutine will loop 10 times, each time asynchronously wait 1 second, then yield a random number between 0 and 10. Use the random module.

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+"""coroutine called async_generator that takes no arguments"""
+import asyncio
+import random
+
+
+async def async_generator():
+    """coroutine will loop 10 times"""
+    for last_project_of_the_week in range(10):
+        await asyncio.sleep(1) in range(10)
+        yield random.uniform(0, 10)
