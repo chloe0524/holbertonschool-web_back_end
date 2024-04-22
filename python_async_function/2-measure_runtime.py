712d4efc1a36c08cd131b21cf9de6159fcfc2a7b
chloe0524 <chloco31@gmail.com>
2024-04-22 16:09:35 +0200

    removed async

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,7 +8,7 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
     Measures the total execution time
 
@@ -22,5 +22,5 @@ async def measure_time(n: int, max_delay: int) -> float:
     start_time = time.time()
     asyncio.run(wait_n(n, max_delay))
     time_over = time.time()
-    total_time = timeover - startingblock
+    total_time = time_over - start_time
     return float(total_time / n)
