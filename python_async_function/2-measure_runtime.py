595075dcaefe394000555b80b825a2f23ae802c9
chloe0524 <chloco31@gmail.com>
2024-04-22 15:44:50 +0200

    trying to get missing point, added intermediary var

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,6 +3,19 @@
 
 import asyncio
 import random
+import time
+
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """Create measure_time to time wait_n + return total_time/n"""
+    #!/usr/bin/env python3
+"""measure_time with n + max_delay andmeasures total exec time """
+
+import asyncio
+import random
 import timeit
 
 
@@ -15,4 +28,5 @@ async def measure_time(n: int, max_delay: int) -> float:
     await wait_n(n, max_delay)
     timeover = timeit.default_timer()
     total_time = timeover - startingblock
-    return total_time / n
+    final_time = total_time / n
+    return final_time
