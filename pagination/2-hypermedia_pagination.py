8bc2ebbaf9727073456399be0b6d8985990f31f7
chloe0524 <chloco31@gmail.com>
2024-04-30 13:37:29 +0200

    pycodestyle fix

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -55,7 +55,7 @@ class Server:
             prev_page = page - 1
 
         return {
-            "page_size": page_size,"page": page,
+            "page_size": page_size, "page": page,
             "data": data,
             "next_page": next_page, "prev_page": prev_page,
             "total_pages": total_pages
