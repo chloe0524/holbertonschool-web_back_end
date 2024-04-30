8bd765dd08f293b90c575977f7471db6da3867b6
chloe0524 <chloco31@gmail.com>
2024-04-30 12:26:13 +0200

    added docstring

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 
 
 def index_range(page: int, page_size: int) -> tuple:
+    """return tuple of size two containing start index + end index"""
     start_here = (page - 1) * page_size
     end_here = start_here + page_size
     return start_here, end_here
