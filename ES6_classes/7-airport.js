5744f6da3ec149eb0b1410d2fff2e0d84a88b75a
chloe0524 <chloco31@gmail.com>
2024-05-09 22:44:44 +0200

    task 7: Implement a class named Airport: Constructor attributes: name (String) code (String)

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+export default class Airport {
+    constructor(name, code) {
+      this._name = name;
+      this._code = code;
+    }
+  
+    get name() {
+      return this._name;
+    }
+  
+    get code() {
+      return this._code;
+    }
+  
+    toString(){
+      return `[${this._code}]`;
+    }
+  }
