519174391431a5e3b5692dafbb4927a7fe655d81
chloe0524 <chloco31@gmail.com>
2024-05-09 22:03:29 +0200

    removed class put back function for test checker

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,20 +1,20 @@
 /* eslint-disable */
-import Building from './5-building.js';
+import Building from './5-building';
 
-class SkyHighBuilding extends Building {
-  constructor(sqft, floors) {
-    super(sqft);
-    this._sqft = sqft;
-    this._floors = floors;
-  }
-
-  get floors() {
-    return this._floors;
-  }
-
-  evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
-  }
+export default function SkyHighBuilding(sqft, floors) {
+    const building = new Building(sqft);
+    return {
+        ...building,
+        _sqft: building.sqft,
+        _floors: floors,
+        get sqft() {
+            return this._sqft;
+        },
+        get floors() {
+            return this._floors;
+        },
+        evacuationWarningMessage() {
+            return `Evacuate slowly the ${this._floors} floors.`;
+        }
+    };
 }
-
-export default SkyHighBuilding;
