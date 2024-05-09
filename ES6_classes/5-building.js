2fc26e7c6ff2f85fd774d91fd109b125d9f177fe
chloe0524 <chloco31@gmail.com>
2024-05-09 21:17:04 +0200

    task 5: Implement a class named Building: Constructor attributes: sqft (Number)

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') throw TypeError('sqft must be a number');
+    this._sqft = value;
+  }
+
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
