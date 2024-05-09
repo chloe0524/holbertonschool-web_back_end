27356f7be14cdb2323dd33f6b9ad8a8a4415138f
chloe0524 <chloco31@gmail.com>
2024-05-09 21:40:57 +0200

    created err on purpose for task

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,14 +1,14 @@
 /* eslint-disable */
 export default class Building {
   constructor(sqft) {
+    if (this.constructor !== Building
+      && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
