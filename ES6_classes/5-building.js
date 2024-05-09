635fe4a0c6f137d56381eb938333977ade713481
chloe0524 <chloco31@gmail.com>
2024-05-09 21:21:35 +0200

    removed setter

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,16 +1,11 @@
 /* eslint-disable */
 export default class Building {
   constructor(sqft) {
-    this.sqft = sqft;
+    this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(value) {
-    if (typeof value !== 'number') throw TypeError('sqft must be a number');
-    this._sqft = value;
   }
 
   evacuationWarningMessage() {
