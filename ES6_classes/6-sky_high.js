cc923f49ab520eb0c9055a4193cfc27bf3e6f830
chloe0524 <chloco31@gmail.com>
2024-05-09 21:57:04 +0200

    testing checker with function and not class

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -2,19 +2,20 @@
 import Building from './5-building.js';
 
 function createSkyHighBuilding(sqft, floors) {
-    const building = new Building(sqft);
-    return {
+  const building = new Building(sqft);
+  return {
     ...building,
+    _sqft: building.sqft,
     _floors: floors,
     get sqft() {
-      return building.sqft;
+      return this._sqft;
     },
     get floors() {
       return this._floors;
     },
     evacuationWarningMessage() {
       return `Evacuate slowly the ${this._floors} floors.`;
-    }
+    },
   };
 }
 
