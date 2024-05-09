164843642310bf9e6dcc00102760948c7612605c
chloe0524 <chloco31@gmail.com>
2024-05-09 22:05:53 +0200

    task 6 repush

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,20 +1,14 @@
-/* eslint-disable */
 import Building from './5-building';
 
-export default function SkyHighBuilding(sqft, floors) {
-    const building = new Building(sqft);
-    return {
-        ...building,
-        _sqft: building.sqft,
-        _floors: floors,
-        get sqft() {
-            return this._sqft;
-        },
-        get floors() {
-            return this._floors;
-        },
-        evacuationWarningMessage() {
-            return `Evacuate slowly the ${this._floors} floors.`;
-        }
-    };
-}
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+  get floors() {
+    return this._floors;
+  }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+};
