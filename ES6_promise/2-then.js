dfdd556da2b46bc3abcc4602071cb721a5995818
chloe0524 <chloco31@gmail.com>
2024-05-13 22:02:24 +0200

    changed err handling and fix mistake with catch and success output put together in the code

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,10 @@
 export default function handleResponseFromAPI(promise) {
   return promise.then(() => {
-    console.log('Got a response from the API');
     return { status: 200, body: 'Success' };
   })
-    .catch(() => {
+    .then((response) => {
       console.log('Got a response from the API');
-      return new Error();
-    });
+      return response || new Error('No response');
+    })
+    .catch(() => new Error());
 }
