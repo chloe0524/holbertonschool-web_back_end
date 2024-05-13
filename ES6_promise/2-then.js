e2724517280eb1acb6ecc263bac4b8e4a1865da3
chloe0524 <chloco31@gmail.com>
2024-05-13 22:08:05 +0200

    removed new err handling

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,7 +2,7 @@ export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({ status: 200, body: 'Success' }))
     .then((response) => {
       console.log('Got a response from the API');
-      return response || new Error('No response');
+      return response;
     })
     .catch(() => new Error());
 }
