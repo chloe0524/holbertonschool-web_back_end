ae7db3a0970d18316f96422b496f6b116c4e85ff
chloe0524 <chloco31@gmail.com>
2024-05-13 22:10:16 +0200

    changed upper case Success to lowercase success

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' }))
+  return promise.then(() => ({ status: 200, body: 'success' }))
     .then((response) => {
       console.log('Got a response from the API');
       return response;
