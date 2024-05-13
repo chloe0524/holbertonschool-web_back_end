c6e9bc92fe93ad471580adff9c1f132f03d9f8f5
chloe0524 <chloco31@gmail.com>
2024-05-13 21:46:40 +0200

    Using the prototype below, return a promise. The parameter is a boolean.

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,4 +1,6 @@
-export default (success) => new Promise((resolve, reject) => {
-  const status = 200;
-  return success ? resolve(`status: ${status}, body: ${success}`) : reject(new Error('The fake API is not working currently'));
-});
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    const status = 200;
+    return success ? resolve(`status: ${status}, body: ${success}`) : reject(new Error('The fake API is not working currently'));
+  });
+}
