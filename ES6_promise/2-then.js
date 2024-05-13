3164bfd8c08bd328cd2c452cff68d5b4d8a78cdf
chloe0524 <chloco31@gmail.com>
2024-05-13 21:57:13 +0200

    task 2: handleResponseFromAPI takes a promise, logs a message on resolution, returns a success object on resolve, and an empty Error on reject.

---
+++ ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+export default function handleResponseFromAPI(promise) {
+  return promise.then(() => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'Success' };
+  })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
+}
