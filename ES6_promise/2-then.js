c82ceffbef8893e422f1b9fcec98f98da5f0e55d
chloe0524 <chloco31@gmail.com>
2024-05-13 22:06:03 +0200

    eslint fix

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => {
-    return { status: 200, body: 'Success' };
-  })
+  return promise.then(() => ({ status: 200, body: 'Success' }))
     .then((response) => {
       console.log('Got a response from the API');
       return response || new Error('No response');
