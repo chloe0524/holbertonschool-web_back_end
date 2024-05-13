32f5c47c358f92ff8980d68616a109a16edb6d75
chloe0524 <chloco31@gmail.com>
2024-05-13 21:48:39 +0200

    pushing version without ternary

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,6 +1,8 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    const status = 200;
-    return success ? resolve(`status: ${status}, body: ${success}`) : reject(new Error('The fake API is not working currently'));
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    }
+    reject(new Error('The fake API is not working currently'));
   });
 }
