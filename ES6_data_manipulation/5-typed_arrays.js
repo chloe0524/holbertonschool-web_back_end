000e6b17d12535f051a223797dce610cc40fe090
chloe0524 <chloco31@gmail.com>
2024-05-21 10:43:28 +0200

    task 5: Define createInt8TypedArray to return ArrayBuffer with Int8 value at specific position

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,4 @@
+export default (length, position, value) => {
+  const view = new DataView(new ArrayBuffer(length)); position < 0 || position >= length ? (() => { throw new Error('Position outside range'); })() : view.setInt8(position, value);
+  return view;
+};
