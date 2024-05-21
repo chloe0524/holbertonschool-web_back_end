0c39052a864d9aed574f229d9fc77148bc879fef
chloe0524 <chloco31@gmail.com>
2024-05-21 14:23:29 +0200

    replaced true with 1

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,4 @@
 export default (map) => {
   if (!(map instanceof Map)) throw new Error('Cannot process');
-  map.forEach((value, key) => (value === true ? map.set(key, 100) : null));
+  map.forEach((value, key) => (value === 1 ? map.set(key, 100) : null));
 };
