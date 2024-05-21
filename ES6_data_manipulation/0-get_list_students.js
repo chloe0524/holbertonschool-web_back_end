dd13abc8b429112f11c59f46f5d418fcc7e86cf1
chloe0524 <chloco31@gmail.com>
2024-05-21 09:56:58 +0200

    task 0: Define getListStudents to return an array of student objects with id, firstName, and location

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export default () => {
+  const person = [{ id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }];
+  return person;
+};
